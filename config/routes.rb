24515c98c0007c6d74107ce5f9531f1e5eac2ece
huojun <huojun_hr@sohu.com>
2017-01-21 21:26:43 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,13 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
+      resources :resumes
     end
   end
 
   resources :jobs do
     resources :resumes
   end
-
-  resources :resumes
 
   root 'jobs#index'
 end
